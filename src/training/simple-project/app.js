0f5cd7283bbaa6e5d8a128703342584ce211b56e
Michal Gebarowski <vmaaik@gmail.com>
2018-10-08 22:12:44 +0100

    add init list to display hits;

--- src/training/simple-project/app.js
+++ src/training/simple-project/app.js
@@ -2,6 +2,8 @@ new Vue({
   el: '#app',
   data: {
     isPanelHidden: true,
+    isListHidden: true,
+    info: [],
     player1: {
       health: 78
     },
@@ -14,9 +16,21 @@ new Vue({
   },
   methods: {
     startGame() {
-      if (this.isPanelHidden) {
-        this.isPanelHidden = false;
-      }
+      this.isPanelHidden = false;
+      this.player1.health = 100;
+      this.player2.health = 100;
+    },
+    attack() {
+      let player1Attack = this.player1.health -= Math.floor((Math.random() * 8) + 1);
+      let player2Attack = this.player2.health -= Math.floor((Math.random() * 18) + 1);
+
+      this.info.push("Player 1 hits Player 2 for " + player1Attack);
+      this.info.push("Player 2 hits Player 1 for " + player2Attack);
+      this.isListHidden = false;
+      return player1Attack, player2Attack;
     }
-  }
+
+  },
+  computed: {}
+
 })
