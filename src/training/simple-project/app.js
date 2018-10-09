4206b4e0a65e07b4ca380092e6c673df4778f6fd
Michal Gebarowski <vmaaik@gmail.com>
2018-10-09 20:49:50 +0100

    implement attack methods; add computed property;

--- src/training/simple-project/app.js
+++ src/training/simple-project/app.js
@@ -1,36 +1,55 @@
 new Vue({
   el: '#app',
   data: {
-    isPanelHidden: true,
-    isListHidden: true,
+    isGameStarted: false,
     info: [],
     player1: {
       health: 78
     },
 
-    player2: {
+    rambo: {
       health: 100
     }
-
-
   },
   methods: {
     startGame() {
-      this.isPanelHidden = false;
+      this.isGameStarted = true;
       this.player1.health = 100;
-      this.player2.health = 100;
+      this.rambo.health = 100;
     },
     attack() {
-      let player1Attack = this.player1.health -= Math.floor((Math.random() * 8) + 1);
-      let player2Attack = this.player2.health -= Math.floor((Math.random() * 18) + 1);
-
-      this.info.push("Player 1 hits Player 2 for " + player1Attack);
-      this.info.push("Player 2 hits Player 1 for " + player2Attack);
-      this.isListHidden = false;
-      return player1Attack, player2Attack;
-    }
-
+      this.player1.health -= this.calculateDamage(false);
+      this.rambo.health -= this.calculateDamage(false);
+      this.checkResult;
+    },
+    specialAttack() {
+      this.player1.health -= this.calculateDamage(false);
+      this.rambo.health -= this.calculateDamage(true);
+      this.checkResult;
+    },
+    heal() {
+    },
+    giveUp() {
+      if (confirm("Click OK to start new game otherwise click CANCEL")) {
+        this.startGame();
+      } else {
+        this.isGameStarted = false;
+      }
+    },
+    calculateDamage(isSpecialAttack) {
+      return (isSpecialAttack) ? Math.floor((Math.random() * 50) + 1) : Math.floor((Math.random() * 15) + 1);
+    },
   },
-  computed: {}
 
-})
+  computed: {
+    checkResult() {
+      if (this.rambo.health < 0) {
+        this.isGameStarted = false;
+        alert('You won!')
+      } else if (this.player1.health < 0) {
+        this.isGameStarted = false;
+        alert('You lose!')
+      }
+    }
+  }
+});
